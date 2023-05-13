d4b5974eabec4208f7f44f50f06fc9124f51d5b6
bkljn4g <bkljn4g@gmail.com>
2023-05-14 00:07:42 +0300

    Промежуточное сохранение

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Anka on 13.05.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+}
