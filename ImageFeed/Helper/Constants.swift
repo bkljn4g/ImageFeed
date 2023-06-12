8e884514c4076ab0e7dbbd58eb597e88851ad8ae
Ann <bkljn4g@gmail.com>
2023-06-12 19:03:40 +0300

    Добавлен файл Constants.swift

---
+++ ImageFeed/Helper/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Ann Goncharova on 04.06.2023.
+//
+
+import Foundation
+
+let AccessKey = "73XJ5JJEmKcgFSqsDNbCbEGKCu6_tfTIykX0oriKdRQ"
+let SecretKey = "LY0A4Mc4U4hLtaW8QPShoCAIbfQ_yjGFau367k5vybE"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
+
