6555e6af3292df36415cfed1c0046153bff15f3c
Ann <bkljn4g@gmail.com>
2023-06-15 05:25:38 +0300

    Незначительные изменения (добавлен класс ProfileService)

---
+++ ImageFeed/Profile/ProfileService.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileService.swift
+//  ImageFeed
+//
+//  Created by Ann Goncharova on 15.06.2023.
+//
+
+import UIKit
+
+final class ProfileService {
+    struct ProfileResult: Codable {
+        
+    }
+}
